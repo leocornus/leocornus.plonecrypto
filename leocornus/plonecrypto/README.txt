a25ca969db702d8c9a301d7b19d29d9de5867fe0
Sean Chen <sean.chen@leocorn.com>
2010-04-13 19:26:50 +0000

    update documentations for release 0.1 fix classifiers in setup.py

--- leocornus/plonecrypto/README.txt
+++ leocornus/plonecrypto/README.txt
@@ -1,3 +1,19 @@
 leocornus.plonecrypto
 =====================
 
+leocornus.plonecrypto is a cryptographic tool for Plone developers to
+easily use cryptography in a Plone application. It supports authentication
+and encryption with both symmetric an asymmetric keys.
+
+Check leocornus.plonecrypto homepage
+(http://plonexp.leocorn.com/xp/leocornus.plonecrypto) for more details.
+
+Dependences
+===========
+
+leocornus.plonecrypto depends on the following Python modules:
+
+* keyczar-python (http://code.google.com/p/keyczar/)
+* PyCrypto (http://www.amk.ca/python/code/crypto)
+* PyAsn1 (http://pyasn1.sourceforge.net/)
+* simplejson - 2.0.9 (http://pypi.python.org/pypi/simplejson/2.0.9)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open(os.path.join('leocornus/plonecrypto', 'version.txt')).read()
+version = open(os.path.join('leocornus/plonecrypto', 'version.txt')).read().split('\n')[0]
 
 setup(name='leocornus.plonecrypto',
       version=version,
@@ -10,11 +10,18 @@ setup(name='leocornus.plonecrypto',
                        open(os.path.join("leocornus/plonecrypto", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Zope :: Plone",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules :: Plone Tool",
+        "Programming Language :: Zope",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
         ],
-      keywords='Python Plone Zope cryptography',
+      keywords='Python Plone Zope Cryptography',
       author='Sean Chen',
       author_email='sean.chen@leocorn.com',
       url='http://plonexp.leocorn.com/xp/leocornus.plonecrypto',
